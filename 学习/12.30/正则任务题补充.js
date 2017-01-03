3edc4f6358190d05657e8dc82a5bcee83d20d1d3
HARRISKING <llc13588440410@outlook.com>
2017-01-03 09:13:50 +0800

    1.3

--- 学习/12.30/正则任务题补充.js
+++ 学习/12.30/正则任务题补充.js
@@ -1,62 +1,21 @@
-var Utils = {
+<script>
+function hasClass(ele, cls){
+    return !!ele.className.match(new RegExp('\\b'+cls+'\\b'));
+},
+function addClass(ele, cls){
+    if(Utils.hasClass(ele,cls)) return;
+    ele.className += ' ' + cls;
+},
 
-    hasClass: function(ele, cls){
-        return !!ele.className.match(new RegExp('\\b'+cls+'\\b'));
-    },
-
-    addClass:function(ele, cls){
-      if(ele.length && ele.length > 0){
-          for(var i=0; i<ele.length; i++){
-              Utils.singleAddClass(ele[i], cls);
-          }
-      }else{
-          Utils.singleAddClass(ele, cls);
-      }
-    },
-
-    removeClass:function(ele, cls){
-       if(ele.length && ele.length > 0){
-          for(var i=0; i<ele.length; i++){
-              Utils.singleRemoveClass(ele[i], cls);
-          }
-      }else{
-          Utils.singleRemoveClass(ele, cls);
-      }
-    },
-
-    singleAddClass: function(ele, cls){
-        if(Utils.hasClass(ele,cls)) return;
-        ele.className += ' ' + cls;
-    },
-
-    singleRemoveClass: function(ele, cls){
-        ele.className = ele.className.replace(new RegExp('\\b'+cls+'\\b', 'g'), '');
-    },
-
-    indexOf: function(ele){
-        var parent = ele.parentElement,
-            siblings = parent.children;
-        for(var i=0; i<siblings.length; i++){
-            if(ele === siblings[i]) return i;
-        }
-        return -1;
+function removeClass(ele, cls){
+    ele.className = ele.className.replace(new RegExp('\\b'+cls+'\\b', 'g'), '');
+},
+indexOf: function(ele){
+    var parent = ele.parentElement,
+        siblings = parent.children;
+    for(var i=0; i<siblings.length; i++){
+        if(ele === siblings[i]) return i;
     }
-};
-
-
-
-var tabCt = document.querySelector('.tabs');
-
-tabCt.addEventListener('click', function(e){
-   var target = e.target,
-       tabs = tabCt.children,
-       index = Utils.indexOf(target),
-       panels = document.querySelectorAll('.panel');
-
-   if(index > -1){
-       Utils.removeClass(tabs, 'active');
-       Utils.addClass(target, 'active');
-       Utils.removeClass(panels, 'active');
-       Utils.addClass(panels[index], 'active');
-   }
-});
+    return -1;
+}
+</script>
