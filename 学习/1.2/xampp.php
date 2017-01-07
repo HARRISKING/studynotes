42f68438f4fb1a0731429f5b97048b2308a6b0f4
HARRISKING <llc13588440410@outlook.com>
2017-01-07 15:48:41 +0800

    1.7

---
+++ 学习/1.2/xampp.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $x="This is my first code!"
+      echo $x;
+    ?>
+  </body>
+</html>
